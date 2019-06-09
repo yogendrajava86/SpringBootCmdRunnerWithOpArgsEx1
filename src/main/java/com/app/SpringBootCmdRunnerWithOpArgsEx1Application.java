9d69f5b2e2391b296dcd4f3539e643a543406823
yogendrajava86 <yogendrajava86@gmail.com>
2019-06-09 15:13:36 +0530

    Initial Setup

--- src/main/java/com/app/SpringBootCmdRunnerWithOpArgsEx1Application.java
+++ src/main/java/com/app/SpringBootCmdRunnerWithOpArgsEx1Application.java
@@ -9,5 +9,6 @@ public class SpringBootCmdRunnerWithOpArgsEx1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCmdRunnerWithOpArgsEx1Application.class, args);
 	}
+	
 
 }
